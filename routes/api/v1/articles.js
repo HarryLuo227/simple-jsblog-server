70ccf28897def72f9897f837acab5672620b6681
HarryLuo227 <fanlos022753@gmail.com>
2024-03-26 16:07:31 +0800

    [docs]: Add swagger document of articles api

--- routes/api/v1/articles.js
+++ routes/api/v1/articles.js
@@ -3,6 +3,49 @@ const express = require('express');
 const router = express.Router();
 const articleService = require('../../../services/articles');
 
+/**
+ * @swagger
+ * components:
+ *   ArticlesWithTagsModel:
+ *     type: object
+ *     properties:
+ *       id:
+ *         type: integer
+ *         default: 1
+ *       title:
+ *         type: string
+ *         default: test-article
+ *       description:
+ *         type: string
+ *         default:
+ *       content:
+ *         type: string
+ *         default: Contents of the test article
+ *       created_at:
+ *         type: string
+ *         format: date-time
+ *       created_by:
+ *         type: string
+ *         default: tester
+ *       modified_at:
+ *         type: string
+ *         format: date-time
+ *       tagsId:
+ *         type: array
+ *         default: [1, 2, 3]
+ */
+
+/**
+ * @swagger
+ * /api/v1/articles:
+ *   get:
+ *     description: List all articles with their corresponding tags name
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Internal Server Error
+ */
 router.get('/', async (req, res) => {
     try {
         logger.debug('Get all articles with their corresponding tags name');
@@ -16,6 +59,53 @@ router.get('/', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/v1/articles:
+ *   post:
+ *     description: Create a new article
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 default: test-article
+ *               description:
+ *                 type: string
+ *                 default:
+ *               content:
+ *                 type: string
+ *                 default: Contents of the test article
+ *               createdBy:
+ *                 type: integer
+ *                 default: 1
+ *               tagsId:
+ *                 type: array
+ *                 default: [1, 2, 3]
+ *     responses:
+ *       201:
+ *         description: Create success and Return the object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/ArticlesWithTagsModel'
+ *       404:
+ *         description: Create failure and Return json object with error message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/ErrorResponse'
+ *       500:
+ *         description: Create failure and Return json object with error message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/ErrorResponse'
+ */
 router.post('/', async (req, res) => {
     try {
         logger.debug('Create a new article');
@@ -42,6 +132,29 @@ router.post('/', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/v1/tags/{articleId}:
+ *   get:
+ *     description: Get article details by article id
+ *     parameters:
+ *       - in: path
+ *         name: articleId
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Success and Return the json object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/ArticlesWithTagsModel'
+ *       500:
+ *         description: Failure and Return json object with error message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/ErrorResponse'
+ */
 router.get('/:id', async (req, res) => {
     try {
         logger.debug('Get article details by article id');
@@ -55,6 +168,54 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/v1/articles/{articleId}:
+ *   put:
+ *     description: Update article details by article id
+ *     parameters:
+ *       - in: path
+ *         name: articleId
+ *         required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 default: test-article
+ *               description:
+ *                 type: string
+ *                 default:
+ *               content:
+ *                 type: string
+ *                 default: Contents of the test article
+ *               tagsId:
+ *                 type: array
+ *                 default: [1, 2, 3]
+ *     responses:
+ *       200:
+ *         description: Update success and Return the updated json object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/ArticlesWithTagsModel'
+ *       400:
+ *         description: Update failure and Return json object with error message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/ErrorResponse'
+ *       500:
+ *         description: Update failure and Return json object with error message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/ErrorResponse'
+ */
 router.put('/:id', async (req, res) => {
     try {
         logger.debug('Update article details by article id');
@@ -77,6 +238,31 @@ router.put('/:id', async (req, res) => {
     }
 });
 
+/**
+ * @swagger
+ * /api/v1/articles/{articleId}:
+ *   delete:
+ *     description: Delete article by article id
+ *     parameters:
+ *       - in: path
+ *         name: articleId
+ *         required: true
+ *     responses:
+ *       204:
+ *         description: Delete success and Return nothing
+ *       404:
+ *         description: Delete failure and Return json object with error message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/ErrorResponse'
+ *       500:
+ *         description: Delete failure and Return json object with error message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/ErrorResponse'
+ */
 router.delete('/:id', async (req, res) => {
     try {
         logger.debug('Delete article by article id');
